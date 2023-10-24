cd7cd6ac687bcc78d228a103b4975362bb1253d1
Kelvin Gothman <121645537+kmgothman@users.noreply.github.com>
2023-10-24 13:23:18 -0500

    Update navigation.js

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -22,9 +22,9 @@ const {currentTheme } = useContext(ThemeContext)
 	<NavigationContainer>
 		
 		<LinkContainer ><b>01</b><StyledLink onClick={props.navClick} to="/">HOME</StyledLink></LinkContainer>
-		<LinkContainer ><b>02</b><StyledLink onClick={props.navClick} to="/Projects">PROJECTS</StyledLink></LinkContainer>
-		<LinkContainer ><b>03</b><StyledLink onClick={props.navClick} to="/About">ABOUT</StyledLink></LinkContainer>
-		<LinkContainer ><b>04</b><StyledLink onClick={props.navClick} to="/Contact">CONTACT</StyledLink></LinkContainer>
+		<LinkContainer ><b>02</b><StyledLink onClick={props.navClick} to="/projects">PROJECTS</StyledLink></LinkContainer>
+		<LinkContainer ><b>03</b><StyledLink onClick={props.navClick} to="/about">ABOUT</StyledLink></LinkContainer>
+		<LinkContainer ><b>04</b><StyledLink onClick={props.navClick} to="/contact">CONTACT</StyledLink></LinkContainer>
 	</NavigationContainer>
 	)
 	
